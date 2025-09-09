680514a00804ff210375d14a599369fed83b22ae
Yunji458 <yunji458@naver.com>
2025-09-09 14:05:44 +0900

    viewresolver

---
+++ 07_springboot/chap04-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/ResolverController.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.viewresolver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResolverController {
+    @GetMapping("string")
+    public String stringReturning(Model model) {
+        model.addAttribute("forwardMMessage", "문자열로 뷰 이름 반환함...");
+        return "result";
+    }
+}
