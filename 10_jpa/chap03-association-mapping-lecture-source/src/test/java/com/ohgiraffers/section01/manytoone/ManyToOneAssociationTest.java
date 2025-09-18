60357d1f7fcbf404a43abb85c666298a2ca611d0
User <yunji458@naver.com>
2025-09-18 11:22:33 +0900

    Merge origin/main (allow unrelated histories)

---
+++ 10_jpa/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/ManyToOneAssociationTest.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.section01.manytoone;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class ManyToOneAssociationTest {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void 다대일_연관관계_객체_그래프_탐색을_이용한_조회_테스트() {
+        int menuCode = 15;
+
+        MenuAndCategory foundMenu = entityManager.find(MenuAndCategory.class, menuCode);
+        System.out.println("foundMenu = " + foundMenu);
+    }
+}
