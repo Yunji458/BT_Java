9332ab4e7ba802aa876601b1567725a45dcc3fa8
User <yunji458@naver.com>
2025-09-18 11:50:08 +0900

    Merge origin/main (allow unrelated histories)

--- 10_jpa/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/ManyToOneAssociationTest.java
+++ 10_jpa/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/ManyToOneAssociationTest.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.section01.manytoone;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.*;
 
 public class ManyToOneAssociationTest {
@@ -33,7 +34,21 @@ public class ManyToOneAssociationTest {
     public void 다대일_연관관계_객체_그래프_탐색을_이용한_조회_테스트() {
         int menuCode = 15;
 
+        /* 설명. 만약에 메뉴만 보고 싶다면?
+         *      : FetchType.EAGER가 아닌 FetchType.LAZY로 바꿔 주어야 한다.
+         *      조인도 하고 싶다면?
+         *      : jpql을 활용한 fetch join을 진행하면 된다.
+         * */
+        /* 설명. 1. 메뉴만 조회한 경우 */
         MenuAndCategory foundMenu = entityManager.find(MenuAndCategory.class, menuCode);
         System.out.println("foundMenu = " + foundMenu);
+
+        /* 설명. 2. 카테고리와 fetch 조인을 하는 경우 */
+        String jpql = "select m from menu_and_category m join fetch m.category where m.menuCode ="
+                + menuCode;
+        TypedQuery<MenuAndCategory> typeMenu = entityManager.createQuery(jpql, MenuAndCategory.class);
+        MenuAndCategory menu = typeMenu.getSingleResult();
+        System.out.println("menu = " + menu);
+        System.out.println("카테고리만 확인: " + menu.getCategory());
     }
 }
--- 10_jpa/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/MenuAndCategory.java
+++ 10_jpa/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/MenuAndCategory.java
@@ -16,12 +16,20 @@ public class MenuAndCategory {
     @Column(name="menu_price")
     private int menuPrice;
 
-    @ManyToOne                         // 메뉴 -> 카테고리의 전체 카디널리티
-    @JoinColumn(name="category_code")  // FK제약조건이 있는 컬럼(자식 테이블에 있는 컬럼명만 쓴다.)
-    private Category category;         // 메뉴 1개가 카테고리 엔티티 객체 몇개를 가지는지(List<타입>/타입)
+    /* 설명.
+     *  FetchType.LAZY(지연 로딩)와 FetchType.EAGER(즉시 로딩)
+     *  1. FetchType.EAGER는 연관관계를 미리 고려해서 해당 엔티티와 조인하는 쿼리가 작성됨
+     *     (항상 조인)
+     *  2. FetchType.LAZY는 연관관계를 나중에 필요한 시점에 고려하여 따로 select을 날려서
+     *     추가하는 개념(나중에 해당 필드를 접근하면 쿼리가 따로 발생)
+     *     (toString()에서 접근하는 것도 해당되므로 고려할 것)
+     * */
+    @ManyToOne(fetch = FetchType.LAZY) // 메뉴 -> 카테고리의 전체 카디널리티
+    @JoinColumn(name="category_code") // FK제약조건이 있는 컬럼(자식 테이블에 있는 컬럼명만 쓴다.)
+    private Category category;        // 메뉴 1개가 카테고리 엔티티 객체 몇 개를 가지는지(List<타입>/타입)
 
     @Column(name="orderable_status")
-    public String orderableStatus;
+    private String orderableStatus;
 
     public MenuAndCategory() {
     }
@@ -80,7 +88,7 @@ public class MenuAndCategory {
                 "menuCode=" + menuCode +
                 ", menuName='" + menuName + '\'' +
                 ", menuPrice=" + menuPrice +
-                ", category=" + category +
+//                ", category=" + category +
                 ", orderableStatus='" + orderableStatus + '\'' +
                 '}';
     }
